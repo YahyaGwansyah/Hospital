5fe47208452286261935b97cf7fa581d15dd6073
Aldani adi <Aldaniadii05@gmail.com>
2024-05-31 10:24:38 +0700

    aku

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+route::get('/', [HomeController::class, 'home']);
 
-Route::get('/login', function () {
-    return view('login');
+Route::get('/', function () {
+    return view('home.index');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin']);
+
